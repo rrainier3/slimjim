117a7097bfef98dc614b409f6e3300c58302a492
Ray Rainier <rayrainier@yahoo.com>
2017-08-27 11:40:09 -0500

    Debug 1.2

--- index.php
+++ index.php
@@ -7,8 +7,14 @@ require 'src/config/db.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
+    
     $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+    
+    $response->getBody()->write("Hello, $name" . "<br>");
+
+    $dbhost = getenv('DB_HOST');
+    
+    $response->getBody()->write("Hostname: $dbhost");
 
     return $response;
 });
