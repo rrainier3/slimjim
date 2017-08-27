5e0de7731deb3abe972955228f58f73f831b896a
Ray Rainier <rayrainier@yahoo.com>
2017-08-27 07:42:06 -0500

    Debug 1.1

--- src/config/db.php
+++ src/config/db.php
@@ -18,17 +18,25 @@
         //     $this->$dbname = getenv('DB_DATABASE');
         // }
 
+        private $dbhost;
+
+        if (isset($_ENV["DB_HOST"])) 
+            $dbhost = $_ENV["DB_HOST"]; 
+        else $dbhost = "ERROR: NO DB_HOST"; 
+
+        echo $dbhost;
+
         // Properties
-        private $dbhost = 'localhost';
-        private $dbuser = 'root';
-        private $dbpass = '123456';
-        private $dbname = 'slimapp';
+        // private $dbhost = 'localhost';
+        // private $dbuser = 'root';
+        // private $dbpass = '123456';
+        // private $dbname = 'slimapp';
 
         // Connect
-        public function connect(){
-            $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
-            $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
-            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $dbConnection;
-        }
+        // public function connect(){
+        //     $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
+        //     $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+        //     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //     return $dbConnection;
+        // }
     }
