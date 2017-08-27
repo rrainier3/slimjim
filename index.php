2f0d8c1674bcd9aa9a3016e69b56bb39b5a4d98e
Ray Rainier <rayrainier@yahoo.com>
2017-08-27 16:22:13 -0500

    Debug 1.1

--- index.php
+++ index.php
@@ -8,18 +8,18 @@ require 'src/config/db.php';
 // Customer Routes
 require 'src/routes/customers.php';
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
+// $app = new \Slim\App;
+// $app->get('/hello/{name}', function (Request $request, Response $response) {
     
-    $name = $request->getAttribute('name');
+//     $name = $request->getAttribute('name');
     
-    $response->getBody()->write("Hello, $name" . "<br>");
+//     $response->getBody()->write("Hello, $name" . "<br>");
 
-    // $dbhost = getenv('DB_HOST');
+//     // $dbhost = getenv('DB_HOST');
     
-    // $response->getBody()->write("Hostname: $dbhost");
+//     // $response->getBody()->write("Hostname: $dbhost");
 
-    return $response;
-});
+//     return $response;
+// });
 
 $app->run();
