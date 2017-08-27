abef0f23ef5734454df7bd9f2b3cfdfe48f82d31
Ray Rainier <rayrainier@yahoo.com>
2017-08-27 07:52:07 -0500

    Debug 1.1

--- src/config/db.php
+++ src/config/db.php
@@ -1,42 +1,21 @@
 <?php
     class db{
-
-        // Get Env Properties
-        // protected $dbhost = getenv(DB_HOST);
-        // protected $dbuser = getenv(DB_USER);
-        // protected $dbpass = getenv('DB_PASSWORD');
-        // protected $dbname = getenv('DB_DATABASE');
-
-        // echo "{$dbhost}";
-        // function __construct() {
-
-        //     $this->$dbhost = getenv('DB_HOST');
-        //     echo ($this->$dbhost);
-
-        //     $this->$dbuser = getenv('DB_USER');
-        //     $this->$dbpass = getenv('DB_PASSWORD');
-        //     $this->$dbname = getenv('DB_DATABASE');
-        // }
-
-        private $dbhost;
-
-        if (isset($_ENV["DB_HOST"])) 
-            $dbhost = $_ENV["DB_HOST"]; 
-        else $dbhost = "ERROR: NO DB_HOST"; 
-
-        echo $dbhost;
-
         // Properties
-        // private $dbhost = 'localhost';
-        // private $dbuser = 'root';
-        // private $dbpass = '123456';
-        // private $dbname = 'slimapp';
+        private $server = $_ENV["DB_HOST"];
+
+        //echo $server;
+        print($server);
+
+        private $dbhost = 'localhost';
+        private $dbuser = 'root';
+        private $dbpass = '123456';
+        private $dbname = 'slimapp';
 
         // Connect
-        // public function connect(){
-        //     $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
-        //     $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
-        //     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //     return $dbConnection;
-        // }
+        public function connect(){
+            $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
+            $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $dbConnection;
+        }
     }
