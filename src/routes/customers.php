634a2e4905709eac319c064f1cb615854f81213d
Ray Rainier <rayrainier@yahoo.com>
2017-08-27 21:10:05 -0500

    Debug 1.1

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -37,12 +37,14 @@ $app->get('/api/customers', function(Request $request, Response $response){
         $dbname = getenv('DB_DATABASE');
 
         echo $dbhost;
+        echo "<br>";
         echo $dbname;
+        echo "<br>";
 
         $mysql_connect_str = "mysql:host={$dbhost};dbname={$dbname}";
 
         echo $mysql_connect_str;
-        
+
         // $$db = new PDO($mysql_connect_str, $dbuser, $dbpass);
         // $$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
